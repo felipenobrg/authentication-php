5459899f16c9a15e6f3b8f0cf9d6d37088c919ec
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-10-25 07:15:57 -0300

    feat: improving validation

--- TRABALHOWEB/src/validatingRegistrationForm.js
+++ TRABALHOWEB/src/validatingRegistrationForm.js
@@ -1,48 +1,47 @@
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-      const passwordRegex = /^.{8,}$/;
+const passwordRegex = /^.{8,}$/;
 
-      document.getElementById('registration-form').addEventListener('submit', function (event) {
-        const emailInput = document.getElementById('input-email');
-        const passwordInput1 = document.getElementById('input-password');
-        const passwordInput2 = document.getElementById('exampleInputPassword2');
+document.getElementById('registration-form').addEventListener('submit', function (event) {
+  const emailInput = document.getElementById('input-email');
+  const passwordInput1 = document.getElementById('input-password');
+  const passwordInput2 = document.getElementById('input-confirm-password');
 
-        if (!document.getElementById('registration-form').checkValidity()) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'Por favor, preencha os campos corretamente.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-          event.preventDefault();
-        }
+  if (!document.getElementById('registration-form').checkValidity()) {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Por favor, preencha os campos corretamente.',
+      icon: 'error',
+      confirmButtonText: 'OK'
+    });
+    event.preventDefault();
+  }
 
-        if (!emailRegex.test(emailInput.value)) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'Por favor, insira um email válido.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-          event.preventDefault();
-        }
+  if (!emailRegex.test(emailInput.value)) {
+    Swal.fire({
+      title: 'Erro!',
+      text: 'Por favor, insira um email válido.',
+      icon: 'error',
+      confirmButtonText: 'OK'
+    });
+    event.preventDefault();
+  }
 
-        if (!passwordRegex.test(passwordInput1.value)) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'A senha deve ter no mínimo 8 caracteres.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-          event.preventDefault();
-        }
+  if (!passwordRegex.test(passwordInput1.value)) {
+    Swal.fire({
+      title: 'Erro!',
+      text: 'A senha deve ter no mínimo 8 caracteres.',
+      icon: 'error',
+      confirmButtonText: 'OK'
+    });
+    event.preventDefault();
+  }
 
-        if (passwordInput1.value !== passwordInput2.value) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'As senhas não coincidem.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-          event.preventDefault();
-        }
-      });
+  if (passwordInput1.value === passwordInput2.value) {
+    Swal.fire({
+      title: 'Erro!',
+      text: 'As senhas não coincidem.',
+      icon: 'error',
+      confirmButtonText: 'OK'
+    });
+  }
+});
