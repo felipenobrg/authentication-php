46e1c2b5428a7d57cfcc92e61572939b3bc16959
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-11-08 07:16:16 -0300

    feat: creating connection to database

--- TRABALHOWEB/src/registration.php
+++ TRABALHOWEB/src/registration.php
@@ -1,99 +1,105 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Cadastro</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      crossorigin="anonymous"
-    />
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-      crossorigin="anonymous"
-    ></script>
 
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
-    />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap"
-      rel="stylesheet"
-    />
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  </head>
-  <body>
-    <div class="input-container">
-      <h1 class="login-h1">Cadastro</h1>
-      <p class="login-p">Digite suas informações para cadastrar sua conta</p>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Cadastro</title>
+  <link rel="stylesheet" href="style.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
 
-      <form id="registration-form" novalidate>
-        <div class="form-group">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="bi bi-person icon"></i>
-              </span>
-              <input
-                type="email"
-                class="form-control"
-                id="input-email"
-                aria-describedby="emailHelp"
-                placeholder="Email"
-                required
-              />
-            </div>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if (empty($email)) {
+      die("Email não pode ser vazio.");
+    }
+
+    $mysqli = require __DIR__ . "/database.php";
+
+    $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+
+    $stmt = $mysqli->stmt_init();
+
+    if (!$stmt->prepare($sql)) {
+      die("Error in SQL" . $mysqli->error);
+    }
+
+    $stmt->bind_param("ss", $email, $password);
+    echo "Before database operation<br>";
+    if ($stmt->execute()) {
+      echo "Success!";
+    } else {
+      echo "Error: " . $stmt->error;
+      echo "Database Error: " . mysqli_error($mysqli);
+    }
+  }
+  ?>
+
+  <div class="input-container">
+    <h1 class="login-h1">Cadastro</h1>
+    <p class="login-p">Digite suas informações para cadastrar sua conta</p>
+
+    <form id="registration-form" action="registration.php" method="POST" novalidate>
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="bi bi-person icon"></i>
+            </span>
+            <input type="email" class="form-control" id="input-email" aria-describedby="emailHelp" placeholder="Email"
+              name="email" required />
           </div>
         </div>
-        <p id="email-error" class="error-message"></p>
+      </div>
+      <p id="email-error" class="error-message"></p>
 
-        <div class="form-group">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="bi bi-lock icon"></i>
-              </span>
-              <input
-                type="password"
-                class="form-control"
-                id="input-password"
-                placeholder="Senha"
-                required
-              />
-            </div>
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="bi bi-lock icon"></i>
+            </span>
+            <input type="password" class="form-control" id="input-password" placeholder="Senha" name="password"
+              required />
           </div>
         </div>
-        <p id="password-error" class="error-message"></p>
-        
-        <div class="form-group">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="bi bi-lock icon"></i>
-              </span>
-              <input
-                type="password"
-                class="form-control"
-                id="input-confirm-password"
-                placeholder="Confirme a senha"
-                required
-              />
-            </div>
+      </div>
+      <p id="password-error" class="error-message"></p>
+
+      <div class="form-group">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="bi bi-lock icon"></i>
+            </span>
+            <input type="password" class="form-control" id="input-confirm-password" placeholder="Confirme a senha"
+              required />
           </div>
         </div>
-        <p id="confirm-error" class="error-message"></p>
+      </div>
+      <p id="confirm-error" class="error-message"></p>
 
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
-      </form>
-    </div>
+      <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+  </div>
 
-    <script src="validatingRegistrationForm.js"></script>
-  </body>
+  <script src="validatingRegistrationForm.js"></script>
+</body>
+
 </html>
--- TRABALHOWEB/src/validatingRegistrationForm.js
+++ TRABALHOWEB/src/validatingRegistrationForm.js
@@ -1,35 +1,38 @@
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const passwordRegex = /^.{8,}$/;
 
-document.getElementById('registration-form').addEventListener('submit', function (event) {
-  const emailInput = document.getElementById('input-email');
-  const passwordInput1 = document.getElementById('input-password');
-  const passwordInput2 = document.getElementById('input-confirm-password');
-  const emailError = document.getElementById('email-error');
-  const passwordError = document.getElementById('password-error');
-  const confirmError = document.getElementById('confirm-error');
+document
+  .getElementById("registration-form")
+  .addEventListener("submit", function (event) {
+    const emailInput = document.getElementById("input-email");
+    const passwordInput1 = document.getElementById("input-password");
+    const passwordInput2 = document.getElementById("input-confirm-password");
+    const emailError = document.getElementById("email-error");
+    const passwordError = document.getElementById("password-error");
+    const confirmError = document.getElementById("confirm-error");
 
-  emailError.textContent = '';
-  passwordError.textContent = '';
-  confirmError.textContent = '';
+    emailError.textContent = "";
+    passwordError.textContent = "";
+    confirmError.textContent = "";
 
-  if (!document.getElementById('registration-form').checkValidity()) {
-    confirmError.textContent = 'Digite as informações corretamente';
-    event.preventDefault();
-  }
+    if (!document.getElementById("registration-form").checkValidity()) {
+      confirmError.textContent =
+        "Por favor, preencha todos os campos corretamente.";
+      event.preventDefault();
+    }
 
-  if (!emailRegex.test(emailInput.value)) {
-    emailError.textContent = 'Por favor, insira um email válido.';
-    event.preventDefault();
-  }
+    if (!emailRegex.test(emailInput.value)) {
+      emailError.textContent = "Por favor, insira um email válido.";
+      event.preventDefault();
+    }
 
-  if (!passwordRegex.test(passwordInput1.value)) {
-    passwordError.textContent = 'A senha deve ter pelo menos 8 caracteres.';
-    event.preventDefault();
-  }
+    if (!passwordRegex.test(passwordInput1.value)) {
+      passwordError.textContent = "A senha deve ter pelo menos 8 caracteres.";
+      event.preventDefault();
+    }
 
-  if (passwordInput1.value !== passwordInput2.value) {
-    confirmError.textContent = 'As senhas não coincidem.';
-    event.preventDefault();
-  }
-});
+    if (passwordInput1.value !== passwordInput2.value) {
+      confirmError.textContent = "As senhas não coincidem.";
+      event.preventDefault();
+    }
+  });
